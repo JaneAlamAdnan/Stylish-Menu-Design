cab5ed030b8a74f1e838c13c9b140cb76e653d5c
adnan <adnannstu@gmail.com>
2021-02-25 15:27:23 +0600

    stylish menu design

---
+++ script.js
@@ -0,0 +1,13 @@
+function show(){
+	document.getElementById("nav-container").classList.toggle('active');
+
+	document.getElementById("loading1").classList.toggle('xxx');
+
+	document.getElementById("line1").classList.toggle('yyy');
+
+	document.getElementById("loading2").classList.toggle('mmm');
+
+	document.getElementById("line2").classList.toggle('nnn');
+
+	document.getElementById("menutext").classList.toggle('aaa');
+}
